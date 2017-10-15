6cea47f777a48bd84dd1ac5c6856c758705e8d94
Fabian Vilers <fabian.vilers@dev-one.com>
2017-10-15 10:33:49 +0200

    Add morgan

--- src/configuration.js
+++ src/configuration.js
@@ -3,6 +3,10 @@
 const configuration = {
   cors: {
     origin: new RegExp(process.env.CORS_ORIGIN || 'http://localhost:4200')
+  },
+  morgan: {
+    format: process.env.MORGAN_FORMAT || 'dev',
+    skip: process.env.MORGAN_SKIP || false
   }
 };
 
--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ const express = require('express');
 const cors = require('cors');
 const compression = require('compression');
 const helmet = require('helmet');
+const morgan = require('morgan');
 const configuration = require('./configuration');
 const app = express();
 
 app.use(cors(configuration.cors));
 app.use(compression());
 app.use(helmet());
+app.use(morgan(configuration.morgan.format, { skip: () => configuration.morgan.skip }));
 app.use('*', (req, res) => res.sendStatus(404));
 
 module.exports = app;
