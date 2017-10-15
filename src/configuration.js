25056f89edf6eb392e19158a3d7aa4247405b535
Fabian Vilers <fabian.vilers@dev-one.com>
2017-10-15 10:35:13 +0200

    Add body parsers

--- src/configuration.js
+++ src/configuration.js
@@ -4,9 +4,15 @@ const configuration = {
   cors: {
     origin: new RegExp(process.env.CORS_ORIGIN || 'http://localhost:4200')
   },
+  json: {
+    limit: process.env.JSON_LIMIT || '25mb'
+  },
   morgan: {
     format: process.env.MORGAN_FORMAT || 'dev',
     skip: process.env.MORGAN_SKIP || false
+  },
+  urlencoded: {
+    extended: false
   }
 };
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const compression = require('compression');
 const helmet = require('helmet');
+const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const configuration = require('./configuration');
 const app = express();
@@ -11,6 +12,8 @@ const app = express();
 app.use(cors(configuration.cors));
 app.use(compression());
 app.use(helmet());
+app.use(bodyParser.json(configuration.json));
+app.use(bodyParser.urlencoded(configuration.urlencoded));
 app.use(morgan(configuration.morgan.format, { skip: () => configuration.morgan.skip }));
 app.use('*', (req, res) => res.sendStatus(404));
 
